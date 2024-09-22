5a2e0ade810db67dcca3f5508bf4c2a1084ac231
Victor-Casado <117583341+Victor-Casado@users.noreply.github.com>
2024-09-22 16:16:59 -0400

    08_teardown

--- 08_teardown/app.py
+++ 08_teardown/app.py
@@ -28,10 +28,11 @@ INVESTIGATIVE APPROACH:
 from flask import Flask
 
 app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs?
+                                         # In Java, when you instantiate a new object, you write Class object = new Class(object0
 
 @app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
-def hello_world():
-    print(__name__)                      # Q2: Where will this print to? Q3: What will it print?
-    return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
+def hello_world():                       # In file systems, it often means the home directory
+    print(__name__)                      # Q2: Where will this print to? Q3: What will it print? It will print to the console, and it will print the str() of the __name__ object
+    return "No hablo queso!"             # Q4: Will this appear anywhere? How u know? It will appear on the web page in your route directory http://ip/
 
-app.run()                                # Q5: Where have you seen similar constructs in other languages?
+app.run()                                # Q5: Where have you seen similar constructs in other languages? This is like calling a method on a java class with no parameters
