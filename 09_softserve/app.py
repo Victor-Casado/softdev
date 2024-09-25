7d884ccde31b338c4be63c6ebca6e2fe2e30d983
Victor Casado <vcasado50@stuy.edu>
2024-09-24 21:16:04 -0400

    added roster and list of professions to 09

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -5,6 +5,27 @@
 #2024-09-23
 #time spent: .5 hours
 import random as r
+
+def getnames():
+    f = open("occupations.csv", "r")
+    string = f.read()
+
+    list = string.split("\n")
+    list = list[1:len(list)-2] #removes example row, total row, and new line row
+
+    names = ""
+#counter = 0
+
+    for i in range(len(list)):
+        if(list[i][0] == "\""):
+            indexcomma = list[i][1:].index("\"") + 2
+            name = list[i][1:indexcomma - 1] #starts at first quote, ends at second quote
+        else:
+            split = list[i].split(",")
+            name = split[0]
+        names+= name + " <br/>"
+    return names
+    
 def pickrand():
     f = open("occupations.csv", "r")
     string = f.read()
@@ -15,7 +36,6 @@ def pickrand():
     names = []
     nums = []
 #counter = 0
-
     for i in range(len(list)):
         if(list[i][0] == "\""):
             indexcomma = list[i][1:].index("\"") + 2
@@ -36,14 +56,13 @@ def pickrand():
 
 
 
-
 from flask import Flask
 app = Flask(__name__)           #create instance of class Flask
 
 @app.route("/")                 #assign fxn to route
 def hello_world():
-    return pickrand()
+    return "The Strawberry Pickers" + "<br/>" + "Victor Casado, Tawab Berri, Jack Blair, and Kishi Wijaya" + "<br/><br/>"+"Selected occupation: " + pickrand() + "<br/><br/><br/>" + getnames()
 
 if __name__ == "__main__":      # true if this file NOT imported
     app.debug = True            # enable auto-reload upon code change
-    app.run()
+    app.run(port=5001)
