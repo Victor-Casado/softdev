470c7dc081a81ec98754a124fbdf5ea4a5536ddc
Victor Casado <vcasado50@stuy.edu>
2024-10-08 10:55:15 -0400

    expanded codebase based on readme

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -52,8 +52,8 @@ def disp_loginpage():
      print(request)
      print("***DIAG: request.args ***")
      print(request.args)
-     print("***DIAG: request.args['username']  ***")
-     print(request.args['username'])
+     #print("***DIAG: request.args['username']  ***")
+     #print(request.args['username'])
      print("***DIAG: request.headers ***")
      print(request.headers)
      return render_template( 'login.html' )
@@ -72,7 +72,8 @@ def authenticate():
      print(request.args['username'])
      print("***DIAG: request.headers ***")
      print(request.headers)
-     return "Waaaa hooo HAAAH"  #response to a form submission
+     return "Your username is: " + request.args['username']
+  #response to a form submission
 
 
 
